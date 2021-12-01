a92d1037c4e8fb7de2d76fc323dd8a9da1babf68
erosMariano <erosmariano1@gmail.com>
2021-11-30 22:26:24 -0300

    Add jogo RPG

---
+++ jogo-rpg-em-react/src/App.styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    background: #24282f;
+    color: #fff;
+    min-height: 100vh;
+`
+
+export const Map = styled.div`
+
+    width: 480px;
+    height: 480px;
+    background-image: url("/assets/map.png");
+    background-position: left top;
+    background-size: 100%;
+`
---
+++ jogo-rpg-em-react/src/components/Character/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div<{left: number, top: number, size: number, sidePos: number}>`
+    width:${props => props.size}px;
+    height:${props => props.size}px;
+    position: relative;
+    left: ${props => props.left}px;
+    top: ${props => props.top}px;
+
+    background-image: url("./assets/char.png");
+
+    background-position: 0px ${props => props.sidePos}px;
+`
